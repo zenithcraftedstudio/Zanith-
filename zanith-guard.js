333ba46acd1f301640c4ae2374f55524df4ab173
zenithcraftedstudio <zenithcraftedstudio@gmail.com>
2026-04-04 12:29:27 +0700

    Update zanith-guard.js

--- zanith-guard.js
+++ zanith-guard.js
@@ -7,7 +7,7 @@
  * ============================================================
  * ⚙️ CHỈ SỬA DÒNG NÀY:
  */
-const ZANITH_SHEET_URL = 'https://script.google.com/macros/s/AKfycby1pe9o__nUO6PIb1XQgAcGuo0n1IDZgMWoO4c6GJ7fmneoUYhSzdmQb-k1UpQLx3Q/exec';
+const ZANITH_SHEET_URL = 'https://script.google.com/macros/s/AKfycbzHkdzlIptdnEaZnXrFXDd60h9bYpC3JlYWsJv07B7R7duRBmZXroXlnyC9yyxmyl9K/exec';
 /**
  * ============================================================
  * HƯỚNG DẪN APPS SCRIPT:
